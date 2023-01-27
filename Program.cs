1d42fdb7f93dde870ae777c5c1ac979a28eac2ee
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-01-26 22:09:27 -0400

    Upload Proyect

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> ints = Enumerable.Range(1, 3);
+            int factorial = ints.Aggregate((f, s) => f * s);
+            Console.WriteLine(factorial);
+        }
+
+    }
+}
